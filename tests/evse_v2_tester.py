0d218f1d00234e27c363585592a7b346789bb377
Olaf Lüke <olaf@tinkerforge.com>
2021-09-01 14:26:05 +0200

    Add EVSE V2 test code

---
+++ tests/evse_v2_tester.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+HOST     = "localhost"
+PORT     = 4223
+
+UID_EVSE = None
+UID_IDAI = "Sii"
+#UID_IDI4 = "T8j"
+UID_IQR1 = "UmP" # CP
+UID_IQR2 = "Ukk" # PP
+UID_IQR3 = "Ukd" # ?
+UID_ICOU = "Gh4"
+UID_IDR  = "Tfh" # L, L'
+
+from tinkerforge.ip_connection import IPConnection
+from tinkerforge.bricklet_evse_v2 import BrickletEVSEV2
+
+from tinkerforge.bricklet_industrial_dual_relay        import BrickletIndustrialDualRelay
+from tinkerforge.bricklet_industrial_dual_analog_in_v2 import BrickletIndustrialDualAnalogInV2
+from tinkerforge.bricklet_industrial_digital_out_4_v2  import BrickletIndustrialDigitalOut4V2
+from tinkerforge.bricklet_industrial_digital_in_4_v2   import BrickletIndustrialDigitalIn4V2
+from tinkerforge.bricklet_industrial_quad_relay_v2     import BrickletIndustrialQuadRelayV2
+from tinkerforge.bricklet_industrial_counter           import BrickletIndustrialCounter
+
+import time
+
+log = print 
+
+class EVSEV2Tester:
+    def __init__(self, log_func = None):
+        if log_func:
+            global log
+            log = log_func
+
+        self.ipcon = IPConnection()
+        self.ipcon.connect(HOST, PORT)
+        self.ipcon.register_callback(IPConnection.CALLBACK_ENUMERATE, self.cb_enumerate)
+        self.ipcon.enumerate()
+
+        log("Trying to find EVSE Bricklet...")
+        while UID_EVSE == None:
+            time.sleep(0.1)
+        log("Found EVSE Bricklet: {0}".format(UID_EVSE))
+
+        self.evse = BrickletEVSEV2(UID_EVSE, self.ipcon)
+    
+        self.idai = BrickletIndustrialDualAnalogInV2(UID_IDAI, self.ipcon)
+        self.idr = BrickletIndustrialDualRelay(UID_IDR,  self.ipcon) 
+#        self.idi4 = BrickletIndustrialDigitalIn4V2(UID_IDI4,   self.ipcon)
+        self.iqr1 = BrickletIndustrialQuadRelayV2(UID_IQR1,    self.ipcon)
+        self.iqr2 = BrickletIndustrialQuadRelayV2(UID_IQR2,    self.ipcon)
+        self.iqr3 = BrickletIndustrialQuadRelayV2(UID_IQR3,    self.ipcon)
+#        self.icou = BrickletIndustrialCounter(UID_ICOU,        self.ipcon)
+        self.idai.set_sample_rate(self.idai.SAMPLE_RATE_61_SPS)
+
+    def cb_enumerate(self, uid, connected_uid, position, hardware_version, firmware_version, device_identifier, enumeration_type):
+        global UID_EVSE
+        if device_identifier == BrickletEVSEV2.DEVICE_IDENTIFIER:
+            UID_EVSE = uid
+
+    # Live = True
+    def set_contactor(self, contactor_input, contactor_output):
+        if contactor_input:
+            self.idr.set_selected_value(0, True)
+            log('AC0 live')
+        else:
+            self.idr.set_selected_value(0, False)
+            log('AC0 off')
+
+        if contactor_output:
+            self.idr.set_selected_value(1, True)
+            log('AC1 live')
+        else:
+            self.idr.set_selected_value(1, False)
+            log('AC1 off')
+
+    def set_diode(self, enable):
+        value = list(self.iqr1.get_value())
+        value[0] = enable
+        self.iqr1.set_value(value)
+        if enable:
+            log("Enable lock switch configuration diode")
+        else:
+            log("Disable lock switch configuration diode")
+
+    def get_cp_pe_voltage(self):
+        return self.idai.get_voltage(1)
+
+    def set_cp_pe_resistor(self, r2700, r880, r240):
+        value = list(self.iqr1.get_value())
+        value[1] = r2700
+        value[2] = r880
+        value[3] = r240
+        self.iqr1.set_value(value)
+
+        l = []
+        if r2700: l.append("2700 Ohm")
+        if r880:  l.append("880 Ohm")
+        if r240:  l.append("240 Ohm")
+    
+        log("Set CP/PE resistor: " + ', '.join(l))
+
+    def set_pp_pe_resistor(self, r1500, r680, r220, r100):
+        value = [r1500, r680, r220, r100]
+        self.iqr2.set_value(value)
+
+        l = []
+        if r1500: l.append("1500 Ohm")
+        if r680:  l.append("680 Ohm")
+        if r220:  l.append("220 Ohm")
+        if r100:  l.append("110 Ohm")
+    
+        log("Set PP/PE resistor: " + ', '.join(l)) 
+
+    def wait_for_contactor_gpio(self, active):
+        if active:
+            log("Waiting for contactor GPIO to become active...")
+        else:
+            log("Waiting for contactor GPIO to become inactive...")
+
+        while True:
+            state = self.evse.get_low_level_state()
+            if state.gpio[9] == active:
+                break
+            time.sleep(0.01)
+
+        log("Done")
+
+    def wait_for_button_gpio(self, active):
+        if active:
+            log("Waiting for button GPIO to become active...")
+        else:
+            log("Waiting for button GPIO to become inactive...")
+
+        while True:
+            state = self.evse.get_low_level_state()
+            if state.gpio[6] == active:
+                break
+            time.sleep(0.1)
+
+        log("Done")
+
+    def shutdown_input_enable(self, enable):
+        self.iqr3.set_selected_value(0, enable)
+    
+    def get_energy_meter_data(self):
+        a = self.evse.get_energy_meter_values()
+        b = self.evse.get_energy_meter_detailed_values()
+        c = self.evse.get_energy_meter_state()
+        return (a, b, c)
+    
+    def get_cp_pe_voltage(self):
+        return self.idai.get_voltage(1)
+
+    def get_pp_pe_voltage(self):
+        return self.idai.get_voltage(0)
+
+if __name__ == "__main__":
+    evse_tester = EVSEV2Tester()
+
+    # Initial config
+    evse_tester.set_contactor(True, False)
+    evse_tester.set_diode(True)
+    evse_tester.set_cp_pe_resistor(False, False, False)
+    evse_tester.set_pp_pe_resistor(False, False, True, False)
+    evse_tester.evse.reset()
+    time.sleep(5)
+
+    while True:
+        time.sleep(5)
+        evse_tester.set_cp_pe_resistor(True, False, False)
+        time.sleep(5)
+        evse_tester.set_cp_pe_resistor(True, True, False)
+
+        evse_tester.wait_for_contactor_gpio(True)
+        evse_tester.set_contactor(True, True)
+
+        time.sleep(10)
+        evse_tester.set_cp_pe_resistor(True, False, False)
+        evse_tester.wait_for_contactor_gpio(False)
+        evse_tester.set_contactor(True, False)
+
+        time.sleep(5)
+        evse_tester.set_cp_pe_resistor(False, False, False)
+        time.sleep(5)
---
+++ tests/full_test.py
@@ -0,0 +1,349 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import time
+import sys
+
+from evse_v2_tester import EVSEV2Tester
+
+def no_log(s):
+    pass
+
+def test_value(value, expected, margin_percent=0.1, margin_absolute=20):
+    if value < 0 and expected > 0:
+        return False
+    if value > 0 and expected < 0:
+        return False
+
+    value = abs(value)
+    expected = abs(expected)
+
+    return (value*(1-margin_percent) - margin_absolute) < expected < (value*(1+margin_percent) + margin_absolute)
+
+if __name__ == "__main__":
+    print('Schaltereinstellung auf 32A stellen')
+    input("Enter drücken...")
+    print('')
+
+    print('Suche EVSE Bricklet 2.0 und Tester')
+    evse_tester = EVSEV2Tester(log_func = no_log)
+    print('... OK')
+
+    data = []
+
+    ident = evse_tester.evse.get_identity()
+    data.append(ident.uid)
+
+    # Initial config
+    evse_tester.set_contactor(True, False)
+    evse_tester.set_diode(True)
+    evse_tester.set_cp_pe_resistor(False, False, False)
+    evse_tester.set_pp_pe_resistor(False, False, True, False)
+    evse_tester.evse.reset()
+
+    print('Warte auf DC-Schutz Kalibrierung (1.5 Sekunden)')
+    print('--> Flackert LED? Wenn nicht kaputt! <--')
+    time.sleep(1.2)
+    print('... OK')
+
+    hw_conf = evse_tester.evse.get_hardware_configuration()
+    print('Teste Jumper-Einstellung')
+    if hw_conf.jumper_configuration != 6:
+        print('Falsche Jumper-Einstellung: {0}'.format(hw_conf.jumper_configuration))
+        print('-----------------> NICHT OK')
+        sys.exit(1)
+    else:
+        print('... OK')
+
+    print('Teste Lock-Switch-Einstellung')
+    if hw_conf.has_lock_switch:
+        print('Falsche Lock-Switch-Einstellung: {0}'.format(hw_conf.has_lock_switch))
+        print('-----------------> NICHT OK')
+        sys.exit(1)
+    else:
+        print('... OK')
+
+
+    print('Teste GPIO Input/Output low')
+    evse_tester.evse.set_gpio_configuration(0, 0, 0)
+    time.sleep(0.1)
+    value = evse_tester.evse.get_low_level_state().gpio[16]
+    if value:
+        print('-----------------> NICHT OK')
+        sys.exit(1)
+    else:
+        print('... OK')
+
+    print('Teste GPIO Input/Output high')
+    evse_tester.evse.set_gpio_configuration(0, 0, 1)
+    time.sleep(0.1)
+    value = evse_tester.evse.get_low_level_state().gpio[16]
+    if not value:
+        print('-----------------> NICHT OK')
+        sys.exit(1)
+    else:
+        print('... OK')
+
+    print('Teste Shutdown Input high')
+    evse_tester.shutdown_input_enable(True)
+    time.sleep(0.1)
+    value = evse_tester.evse.get_low_level_state().gpio[5]
+    if value:
+        print('-----------------> NICHT OK')
+        sys.exit(1)
+    else:
+        print('... OK')
+
+    print('Teste Shutdown Input low')
+    evse_tester.shutdown_input_enable(False)
+    time.sleep(0.1)
+    value = evse_tester.evse.get_low_level_state().gpio[5]
+    if not value:
+        print('-----------------> NICHT OK')
+        sys.exit(1)
+    else:
+        print('... OK')
+
+    print('Teste CP/PE...')
+    print(' * open')
+    res_cppe = evse_tester.evse.get_low_level_state().resistances[0]
+    data.append(str(res_cppe))
+    if res_cppe == 4294967295:
+        print(' * ... OK ({0} Ohm)'.format(res_cppe))
+    else:
+        print('-----------------> NICHT OK {0}'.format(res_cppe))
+        sys.exit(1)
+    vol_cppe = evse_tester.get_cp_pe_voltage()
+    if test_value(vol_cppe, 12213):
+        print(' * ... OK ({0} mV)'.format(vol_cppe))
+    else:
+        print('-----------------> NICHT OK {0}'.format(vol_cppe))
+        sys.exit(1)
+
+    print(' * 2700 Ohm')
+    evse_tester.set_cp_pe_resistor(True, False, False)
+    time.sleep(0.5)
+    res_cppe = evse_tester.evse.get_low_level_state().resistances[0]
+    data.append(str(res_cppe))
+    if test_value(res_cppe, 2700):
+        print(' * ... OK ({0} Ohm)'.format(res_cppe))
+    else:
+        print('-----------------> NICHT OK {0}'.format(res_cppe))
+        sys.exit(1)
+    vol_cppe = evse_tester.get_cp_pe_voltage()
+    if test_value(vol_cppe, 9069):
+        print(' * ... OK ({0} mV)'.format(vol_cppe))
+    else:
+        print('-----------------> NICHT OK {0}'.format(vol_cppe))
+        sys.exit(1)
+
+    print(' * 880 Ohm')
+    evse_tester.set_cp_pe_resistor(True, True, False)
+    time.sleep(0.5)
+    res_cppe = evse_tester.evse.get_low_level_state().resistances[0]
+    data.append(str(res_cppe))
+    if test_value(res_cppe, 880):
+        print(' * ... OK ({0} Ohm)'.format(res_cppe))
+    else:
+        print('-----------------> NICHT OK {0}'.format(res_cppe))
+        sys.exit(1)
+    vol_cppe = evse_tester.get_cp_pe_voltage()
+    if test_value(vol_cppe, 6049):
+        print(' * ... OK ({0} mV)'.format(vol_cppe))
+    else:
+        print('-----------------> NICHT OK {0}'.format(vol_cppe))
+        sys.exit(1)
+
+    print(' * 240 Ohm')
+    evse_tester.set_cp_pe_resistor(True, True, True)
+    time.sleep(0.5)
+    res_cppe = evse_tester.evse.get_low_level_state().resistances[0]
+    data.append(str(res_cppe))
+    if test_value(res_cppe, 240):
+        print(' * ... OK ({0} Ohm)'.format(res_cppe))
+    else:
+        print('-----------------> NICHT OK {0}'.format(res_cppe))
+        sys.exit(1)
+    vol_cppe = evse_tester.get_cp_pe_voltage()
+    if test_value(vol_cppe, 2646):
+        print(' * ... OK ({0} mV)'.format(vol_cppe))
+    else:
+        print('-----------------> NICHT OK {0}'.format(vol_cppe))
+        sys.exit(1)
+
+    evse_tester.set_cp_pe_resistor(False, False, False)
+    time.sleep(0.5)
+
+
+    print('Teste PP/PE...')
+    print(' * 220 Ohm')
+    res_pppe = evse_tester.evse.get_low_level_state().resistances[1]
+    data.append(str(res_pppe))
+    if test_value(res_pppe, 220):
+        print(' * ... OK ({0} Ohm)'.format(res_pppe))
+    else:
+        print('-----------------> NICHT OK {0}'.format(res_pppe))
+        sys.exit(1)
+    vol_pppe = evse_tester.get_pp_pe_voltage()
+    if test_value(vol_pppe, 834):
+        print(' * ... OK ({0} mV)'.format(vol_pppe))
+    else:
+        print('-----------------> NICHT OK {0}'.format(vol_pppe))
+        sys.exit(1)
+
+    print(' * 1500 Ohm')
+    evse_tester.set_pp_pe_resistor(True, False, False, False)
+    time.sleep(0.5)
+    res_pppe = evse_tester.evse.get_low_level_state().resistances[1]
+    data.append(str(res_pppe))
+    if test_value(res_pppe, 1500):
+        print(' * ... OK ({0} Ohm)'.format(res_pppe))
+    else:
+        print('-----------------> NICHT OK {0}'.format(res_pppe))
+        sys.exit(1)
+    vol_pppe = evse_tester.get_pp_pe_voltage()
+    if test_value(vol_pppe, 2313):
+        print(' * ... OK ({0} mV)'.format(vol_pppe))
+    else:
+        print('-----------------> NICHT OK {0}'.format(vol_pppe))
+        sys.exit(1)
+
+    print(' * 680 Ohm')
+    evse_tester.set_pp_pe_resistor(False, True, False, False)
+    time.sleep(0.5)
+    res_pppe = evse_tester.evse.get_low_level_state().resistances[1]
+    data.append(str(res_pppe))
+    if test_value(res_pppe, 680):
+        print(' * ... OK ({0} Ohm)'.format(res_pppe))
+    else:
+        print('-----------------> NICHT OK {0}'.format(res_pppe))
+        sys.exit(1)
+    vol_pppe = evse_tester.get_pp_pe_voltage()
+    if test_value(vol_pppe, 1695):
+        print(' * ... OK ({0} mV)'.format(vol_pppe))
+    else:
+        print('-----------------> NICHT OK {0}'.format(vol_pppe))
+        sys.exit(1)
+
+    print(' * 100 Ohm')
+    evse_tester.set_pp_pe_resistor(False, False, False, True)
+    time.sleep(0.5)
+    res_pppe = evse_tester.evse.get_low_level_state().resistances[1]
+    data.append(str(res_pppe))
+    if test_value(res_pppe, 100):
+        print(' * ... OK ({0} Ohm)'.format(res_pppe))
+    else:
+        print('-----------------> NICHT OK {0}'.format(res_pppe))
+        sys.exit(1)
+    vol_pppe = evse_tester.get_pp_pe_voltage()
+    if test_value(vol_pppe, 441):
+        print(' * ... OK ({0} mV)'.format(vol_pppe))
+    else:
+        print('-----------------> NICHT OK {0}'.format(vol_pppe))
+        sys.exit(1)
+
+    evse_tester.set_pp_pe_resistor(False, False, True, False)
+    time.sleep(0.5)
+
+
+    print('Beginne Test-Ladung')
+
+    evse_tester.set_contactor(True, False)
+    evse_tester.set_diode(True)
+    evse_tester.set_cp_pe_resistor(False, False, False)
+    evse_tester.set_pp_pe_resistor(False, False, True, False)
+    evse_tester.evse.reset()
+    time.sleep(0.5)
+
+    print('Setze 2700 Ohm + 1300 Ohm Widerstand')
+    evse_tester.set_cp_pe_resistor(True, True, False)
+    print('... OK')
+
+    evse_tester.wait_for_contactor_gpio(False)
+
+    print('Aktiviere Schütz (2 Sekunden)')
+    evse_tester.set_contactor(True, True)
+
+    time.sleep(0.5)
+    print('... OK')
+
+    test_voltages = [-10302, -9698, -9095, -8476, -7871, -7272, -6648, -6047, -5445, -4822, -4224, -3617, -3000, -2396]
+    for i, a in enumerate(range(6, 33, 2)):
+        print('Test CP/PE {0}A'.format(a))
+        evse_tester.evse.set_max_charging_current(a*1000)
+        time.sleep(0.5)
+        res_cppe = evse_tester.evse.get_low_level_state().resistances[0]
+        data.append(str(res_cppe))
+        if test_value(res_cppe, 880):
+            print(' * ... OK ({0} Ohm)'.format(res_cppe))
+        else:
+            print('-----------------> NICHT OK {0}'.format(res_cppe))
+            sys.exit(1)
+        vol_cppe = evse_tester.get_cp_pe_voltage()
+        if test_value(vol_cppe, test_voltages[i]):
+            print(' * ... OK ({0} mV)'.format(vol_cppe))
+        else:
+            print('-----------------> NICHT OK {0}'.format(vol_cppe))
+            sys.exit(1)
+
+    print('Teste Stromzähler')
+    values, detailed_values, state = evse_tester.get_energy_meter_data()
+    if (not state.available) or (values.energy_absolute < 1):
+        print('-----------------> NICHT OK: {0}, {1}'.format(str(state), str(values)))
+    else:
+        print('... OK: {0}, {1}'.format(state.available, values.energy_absolute))
+
+
+    print('Ausschaltzeit messen')
+    t1 = time.time()
+    evse_tester.set_cp_pe_resistor(True, False, False)
+    evse_tester.wait_for_contactor_gpio(True)
+    evse_tester.set_contactor(True, False)
+    t2 = time.time()
+
+    delay = int((t2-t1)*1000)
+    data.append(str(delay))
+    print('... OK')
+
+    if delay <= 100:
+        print('Ausschaltzeit: {0}ms OK'.format(delay))
+    else:
+        print('Ausschaltzeit: {0}ms'.format(delay))
+        print('-----------------> NICHT OK')
+        sys.exit(1)
+
+    print('Teste Jumper-Einstellung')
+
+    print('Schaltereinstellung auf "20A" stellen und dann Taster drücken')
+    evse_tester.wait_for_button_gpio(True) # Button True = Pressed
+    print('')
+
+    evse_tester.evse.reset()
+    time.sleep(0.5)
+    hw_conf = evse_tester.evse.get_hardware_configuration()
+    if hw_conf.jumper_configuration != 4:
+        print('Falsche Jumper-Einstellung: {0}'.format(hw_conf.jumper_configuration))
+        print('-----------------> NICHT OK')
+        sys.exit(1)
+    else:
+        print('... OK')
+
+    print('Schaltereinstellung auf "Disabled" stellen und dann Taster drücken')
+    evse_tester.wait_for_button_gpio(True) # Button True = Pressed
+    print('')
+
+    evse_tester.evse.reset()
+    time.sleep(0.5)
+    hw_conf = evse_tester.evse.get_hardware_configuration()
+    if hw_conf.jumper_configuration != 8:
+        print('Falsche Jumper-Einstellung: {0}'.format(hw_conf.jumper_configuration))
+        print('-----------------> NICHT OK')
+        sys.exit(1)
+    else:
+        print('... OK')
+
+    print('')
+    print('Fertig. Alles OK')
+
+    with open('full_test_log.csv', 'a+') as f:
+        f.write(', '.join(data) + '\n')
